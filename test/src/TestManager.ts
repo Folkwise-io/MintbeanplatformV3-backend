13dd1d7e0750ee2e7a767ac1082f4bae306d7443
Jimmy Peng <jimmy.peng@mintbean.io>
2020-08-28 16:03:24 -0400

    Implement TestManager wrapper for apollo-server-testing methods
    The apollo-server-testing package provides the createTestServer hook.
    Passing a server instance to the hook returns "query" and "mutate"
    functions which can then be used to run requests against the server
    instance, without needing to start an HTTP server.

--- test/src/TestManager.ts
+++ test/src/TestManager.ts
@@ -1,1 +1,10 @@
-export default class TestManager {}
+import { createTestClient } from "apollo-server-testing";
+import server from "../../src/server";
+import { Query } from "./createTestClient";
+const { query, mutate } = createTestClient(server);
+
+export default class TestManager {
+  async query(args: Query) {
+    return await query(args);
+  }
+}
--- test/user.test.ts
+++ test/user.test.ts
@@ -1,5 +1,23 @@
-describe("test one", () => {
-  it("does something", () => {
-    expect(1).toBe(1);
+import TestManager from "./src/TestManager";
+import { gql } from "apollo-server-express";
+import { knex } from "../src/db/knex";
+
+const testManager = new TestManager();
+
+describe("Querying users", () => {
+  it("does something", async () => {
+    const GET_ALL_USERS = gql`
+      query getAllUsers {
+        users {
+          firstName
+        }
+      }
+    `;
+    const result = await testManager.query({ query: GET_ALL_USERS });
+    console.log(JSON.stringify(result.data, null, 2));
   });
+});
+
+afterAll(async () => {
+  await knex.destroy();
 });
