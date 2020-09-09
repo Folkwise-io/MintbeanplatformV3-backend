c5195ab7fbc688bb055337833ab36c65a6e90b60
Jimmy Peng <jimmy.peng@mintbean.io>
2020-09-09 15:31:07 -0400

    Move const declarations in user.test
    Originally all declared at the top, they have been moved to more logical
    locations within each `describe` or `it` block.

--- test/user.test.ts
+++ test/user.test.ts
@@ -3,7 +3,7 @@ import { gql } from "apollo-server-express";
 import { User } from "../src/types/gqlGeneratedTypes";
 
 // Will use generator factory / faker once more entities are added
-const AMY = {
+const AMY: User = {
   id: "00000000-0000-0000-0000-000000000000",
   username: "aadams",
   email: "a@a.com",
@@ -23,42 +23,6 @@ const BOB: User = {
   createdAt: "2020-04-15",
 };
 
-const BAD_UUID_QUERY = gql`
-  query badUserId {
-    user(id: "000000") {
-      firstName
-      lastName
-    }
-  }
-`;
-
-const BAD_USERNAME_QUERY = gql`
-  query badUserName {
-    user(username: 5) {
-      firstName
-      lastName
-    }
-  }
-`;
-
-const GET_AMY_QUERY = gql`
-  query getOneUser {
-    user(id: "00000000-0000-0000-0000-000000000000") {
-      firstName
-      lastName
-    }
-  }
-`;
-
-const GET_ALL_USERS_QUERY = gql`
-  query getAllUsers {
-    users {
-      firstName
-      lastName
-    }
-  }
-`;
-
 const testManager = TestManager.build();
 
 beforeEach(async () => {
@@ -67,6 +31,15 @@ beforeEach(async () => {
 
 describe("GraphQL built-in validation", () => {
   it("throws an error when you pass in a username that's not a string", async () => {
+    const BAD_USERNAME_QUERY = gql`
+      query badUserName {
+        user(username: 5) {
+          firstName
+          lastName
+        }
+      }
+    `;
+
     await testManager
       .query({ query: BAD_USERNAME_QUERY })
       .then(testManager.getErrors)
@@ -76,6 +49,15 @@ describe("GraphQL built-in validation", () => {
   });
 
   it("throws an error when you pass in an ID that is not a UUID", async () => {
+    const BAD_UUID_QUERY = gql`
+      query badUserId {
+        user(id: "000000") {
+          firstName
+          lastName
+        }
+      }
+    `;
+
     await testManager
       .query({ query: BAD_UUID_QUERY })
       .then(testManager.getErrors)
@@ -86,10 +68,28 @@ describe("GraphQL built-in validation", () => {
 });
 
 describe("Querying users", () => {
+  const GET_ONE_QUERY = gql`
+    query getOneUser {
+      user(id: "00000000-0000-0000-0000-000000000000") {
+        firstName
+        lastName
+      }
+    }
+  `;
+
+  const GET_ALL_USERS_QUERY = gql`
+    query getAllUsers {
+      users {
+        firstName
+        lastName
+      }
+    }
+  `;
+
   it("gets one user by ID", async () => {
     await testManager
       .addUsers([AMY, BOB])
-      .then(() => testManager.query({ query: GET_AMY_QUERY }))
+      .then(() => testManager.query({ query: GET_ONE_QUERY }))
       .then(testManager.getData)
       .then(({ user }) => {
         expect(AMY).toMatchObject(user);
@@ -110,7 +110,7 @@ describe("Querying users", () => {
   it("gets no users when ID doesn't exist", async () => {
     await testManager
       .addUsers([])
-      .then(() => testManager.query({ query: GET_AMY_QUERY }))
+      .then(() => testManager.query({ query: GET_ONE_QUERY }))
       .then(testManager.getDataAndErrors)
       .then(({ data, errors }) => {
         expect(data.user).toBeNull();
