9895da9be04d0b1cc67670abddd2bd73c36cb124
Jimmy Peng <jimmy.peng@mintbean.io>
2020-09-19 19:41:32 -0400

    Add yup validation to createProject validator

--- src/validator/ProjectResolverValidator.ts
+++ src/validator/ProjectResolverValidator.ts
@@ -1,7 +1,9 @@
+import { UserInputError } from "apollo-server-express";
 import { ServerContext } from "../buildServerContext";
 import ProjectDao from "../dao/ProjectDao";
 import { ProjectServiceAddOneInput, ProjectServiceGetOneArgs } from "../service/ProjectService";
 import { MutationCreateProjectArgs, QueryProjectArgs } from "../types/gqlGeneratedTypes";
+import createProjectInputSchema from "./yupSchemas/createProjectInputSchema";
 
 export default class ProjectResolverValidator {
   constructor(private projectDao: ProjectDao) {}
@@ -11,6 +13,12 @@ export default class ProjectResolverValidator {
 
   async addOne({ input }: MutationCreateProjectArgs): Promise<ProjectServiceAddOneInput> {
     //TODO: Validate createProject input: check if userId exists in db? (only needed if admin requests)
+    // Q: Got TS error when trying to promise-chain
+    try {
+      createProjectInputSchema.validateSync(input);
+    } catch (e) {
+      throw new UserInputError(e.message);
+    }
     return input;
   }
 }
