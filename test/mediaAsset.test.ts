c6c6cefe2cc57ac78d74483193f8a14aebd57e63
Jimmy Peng <jimmy.peng@mintbean.io>
2020-09-20 18:33:37 -0400

    Add edge case tests for querying project media assets

--- test/mediaAsset.test.ts
+++ test/mediaAsset.test.ts
@@ -44,6 +44,7 @@ describe("Querying nested media assets in projects", () => {
       AMY_PAPERJS_MEDIA_ASSET_2_JOIN,
       BOB_PAPERJS_MEDIA_ASSET_1_JOIN,
     ]);
+
     await testManager
       .getGraphQLData({ query: GET_PROJECT_WITH_NESTED_MEDIA_ASSETS, variables: { id: AMY_PAPERJS_PROJECT.id } })
       .then(({ project }) => {
@@ -53,11 +54,38 @@ describe("Querying nested media assets in projects", () => {
         expect(AMY_PAPERJS_MEDIA_ASSET_1).toMatchObject(mediaAsset1);
       });
 
-      await testManager
+    await testManager
       .getGraphQLData({ query: GET_PROJECT_WITH_NESTED_MEDIA_ASSETS, variables: { id: BOB_PAPERJS_PROJECT.id } })
       .then(({ project }) => {
         expect(project.mediaAssets).toHaveLength(1);
         expect(BOB_PAPERJS_MEDIA_ASSET_1).toMatchObject(project.mediaAssets[0]);
+      });
+  });
+
+  it("returns an empty array for media assets if project has no media assets", async () => {
+    await testManager
+      .getGraphQLData({ query: GET_PROJECT_WITH_NESTED_MEDIA_ASSETS, variables: { id: BOB_PAPERJS_PROJECT.id } })
+      .then(({ project }) => {
+        expect(project.mediaAssets).toHaveLength(0);
+      });
+  });
+
+  it("doesn't retrieve deleted media assets", async () => {
+    await testManager.addMediaAssets([
+      { ...AMY_PAPERJS_MEDIA_ASSET_1, deleted: true } as any,
+      AMY_PAPERJS_MEDIA_ASSET_2,
+      BOB_PAPERJS_MEDIA_ASSET_1,
+    ]);
+    await testManager.addProjectMediaAssets([
+      AMY_PAPERJS_MEDIA_ASSET_1_JOIN,
+      AMY_PAPERJS_MEDIA_ASSET_2_JOIN,
+      BOB_PAPERJS_MEDIA_ASSET_1_JOIN,
+    ]);
+
+    await testManager
+      .getGraphQLData({ query: GET_PROJECT_WITH_NESTED_MEDIA_ASSETS, variables: { id: AMY_PAPERJS_PROJECT.id } })
+      .then(({ project }) => {
+        expect(project.mediaAssets).toHaveLength(1);
       });
   });
 });
