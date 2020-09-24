ab7ca6e5bab1096cf8f3ea9c86de1f98df5297e8
Jimmy Peng <jimmy.peng@mintbean.io>
2020-09-24 11:20:57 -0400

    Add depthLimit to apollo-server validationRules

--- src/buildApolloServer.ts
+++ src/buildApolloServer.ts
@@ -1,11 +1,13 @@
 import { ApolloServer } from "apollo-server-express";
 import { GraphQLSchema } from "graphql";
 import { BuildExpressServerContext } from "./buildServerContext";
+import depthLimit from "graphql-depth-limit";
 
 export default function buildApolloServer(schema: GraphQLSchema, context: BuildExpressServerContext): ApolloServer {
   const apolloServer = new ApolloServer({
     schema,
     context,
+    validationRules: [depthLimit(4)],
   });
 
   return apolloServer;
--- test/security.test.ts
+++ test/security.test.ts
@@ -7,6 +7,6 @@ describe("Backend security", () => {
   it("rejects queries that are too nested", async () => {
     await testManager
       .getErrorMessage({ query: FIVE_LEVEL_NESTED_QUERY })
-      .then((errorMessage) => expect(errorMessage).toMatch(/too complex/i));
+      .then((errorMessage) => expect(errorMessage).toMatch(/maximum.*depth/i));
   });
 });
