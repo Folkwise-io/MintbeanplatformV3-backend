c826fc21f7c5f341439f0f096248158e0f9471b1
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-05 17:47:41 -0400

    Send json-ld email successfully

--- src/graphql/resolver/emailResolver.ts
+++ src/graphql/resolver/emailResolver.ts
@@ -11,7 +11,44 @@ const emailResolver = (emailResolverValidator: EmailResolverValidator, emailServ
           to: "jimmy.peng@mintbean.io",
           from: "jimmy.peng@mintbean.io",
           subject: "hi",
-          html: "hi",
+          html: `
+<html>
+  <head>
+    <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "EventReservation",
+      "reservationNumber": "E123456789",
+      "reservationStatus": "http://schema.org/Confirmed",
+      "underName": {
+        "@type": "Person",
+        "name": "John Smith"
+      },
+      "reservationFor": {
+        "@type": "Event",
+        "name": "MB JavaScript Hacks: Quiz App Showdown - https://mintbean.io/meets/3d9c6cc8-4f1b-4ac8-9f6e-2896acad40e1",
+        "startDate": "2020-10-07T19:30:00-08:00",
+        "location": {
+          "@type": "Place",
+          "name": "Mintbean",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Toronto",
+            "addressRegion": "ON"
+          },
+          "url": "https://mintbean.io"
+        }
+      }
+    }
+    </script>
+  </head>
+  <body>
+    <p>
+      This a test for an event schema in Gmail.
+    </p>
+  </body>
+</html>
+`,
         };
         return emailService.sendEmail(testEmail);
       },
