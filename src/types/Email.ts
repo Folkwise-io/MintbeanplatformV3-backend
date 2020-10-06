ed79ef344f73d20aff2f869d9593db3ee9fdfb90
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-06 10:53:44 -0400

    Add typing for attachments

--- src/types/Email.ts
+++ src/types/Email.ts
@@ -1,7 +1,14 @@
-
 export interface Email {
   to: string;
   from: string;
   subject: string;
   html: string;
+  attachments?: Attachment[];
+}
+
+export interface Attachment {
+  content: string;
+  filename: string;
+  type: string;
+  disposition: string;
 }
