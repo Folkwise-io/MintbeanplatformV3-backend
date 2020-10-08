19a794bf19d3dda5c75f2c74a8eed9a9232d7f06
Claire Froelich <claire.froelich@gmail.com>
2020-10-08 10:19:13 -0700

    Add kanbanCards migration

---
+++ src/db/migrations/20201008101702_kanbanCards.ts.ts
@@ -0,0 +1,21 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("kanbanCards", (table) => {
+    table.uuid("id").notNullable().defaultTo(knex.raw("uuid_generate_v4()")).unique();
+    table.uuid("kanbanId").notNullable();
+    table.text("title").notNullable();
+    table.text("body").notNullable();
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+    table.boolean("deleted").notNullable().defaultTo(false);
+
+    // Constraints and indices
+    table.primary(["id"]);
+    table.foreign("kanbanId").references("kanbans.id").onDelete("CASCADE");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("kanbanCards");
+}
