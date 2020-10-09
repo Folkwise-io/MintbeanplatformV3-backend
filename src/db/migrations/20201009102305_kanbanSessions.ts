8b530925355b5ec7f82f23f1302d6bb7646e6fbe
Claire Froelich <claire.froelich@gmail.com>
2020-10-09 12:55:25 -0700

    Add Kanban Session migration

---
+++ src/db/migrations/20201009102305_kanbanSessions.ts
@@ -0,0 +1,23 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("kanbanSessions", (table) => {
+    table.uuid("id").notNullable().defaultTo(knex.raw("uuid_generate_v4()")).unique();
+    table.uuid("kanbanId").notNullable();
+    table.uuid("userId").notNullable();
+    table.uuid("meetId");
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+    table.boolean("deleted").notNullable().defaultTo(false);
+
+    // Constraints and indices
+    table.primary(["id"]);
+    table.foreign("kanbanId").references("kanbans.id").onDelete("CASCADE");
+    table.foreign("userId").references("users.id").onDelete("CASCADE");
+    table.foreign("meetId").references("meets.id").onDelete("SET NULL");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("kanbanSessions");
+}
