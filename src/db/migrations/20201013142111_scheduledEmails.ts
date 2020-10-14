9aa5c7227329f3957116eedb8255c2186f025cd7
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-14 13:10:34 -0400

    Create scheduledEmails schema

---
+++ src/db/migrations/20201013142111_scheduledEmails.ts
@@ -0,0 +1,22 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("scheduledEmails", (table) => {
+    table.uuid("id").notNullable().defaultTo(knex.raw("uuid_generate_v4()")).unique();
+    table.enu("templateName", ["meetRegistration", "signup", "checkInAfterSignup", "all"]).notNullable();
+    table.uuid("userId");
+    table.uuid("meetId");
+    table.timestamp("sendAt").notNullable().defaultTo(knex.fn.now());
+    table.boolean("sent").notNullable().defaultTo(false);
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+
+    // Constraints and indices
+    table.primary(["id"]);
+    table.foreign("userId").references("users.id").onDelete("SET NULL");
+    table.foreign("meetId").references("meets.id").onDelete("SET NULL");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("scheduledEmails");
+}
