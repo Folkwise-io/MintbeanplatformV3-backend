912f2beda3f8b6705cc1e58a4bbbc77d21bf6241
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-15 11:11:52 -0400

    Add scheduledEmails seeds

--- src/db/seeds/11-scheduledEmails.ts
+++ src/db/seeds/11-scheduledEmails.ts
@@ -1,20 +1,33 @@
 import * as Knex from "knex";
-import { nDaysAndHoursFromNowInWallClockTime } from "../../util/timeUtils";
+import { ScheduledEmail } from "../../types/Email";
+import { nDaysAndHoursFromNowInUtcTime } from "../../util/timeUtils";
 export async function seed(knex: Knex): Promise<void> {
   await knex("scheduledEmails").del();
 
-  await knex("scheduledEmails").insert([
+  await knex<ScheduledEmail>("scheduledEmails").insert([
     {
       templateName: "meetRegistration",
       userId: "00000000-0000-0000-0000-000000000000",
       meetId: "00000000-0000-0000-0000-000000000000",
-      sendAt: nDaysAndHoursFromNowInWallClockTime(2),
+      sendAt: nDaysAndHoursFromNowInUtcTime(0, 1),
     },
     {
       templateName: "meetRegistration",
       userId: "00000000-0000-4000-a000-000000000000",
       meetId: "87496d2d-ae36-4039-bd14-45bd0de3929c",
-      sendAt: nDaysAndHoursFromNowInWallClockTime(1),
+      sendAt: nDaysAndHoursFromNowInUtcTime(1),
+    },
+    {
+      templateName: "checkInAfterSignup",
+      userId: "00000000-0000-0000-0000-000000000000",
+      meetId: null,
+      sendAt: nDaysAndHoursFromNowInUtcTime(2),
+    },
+    {
+      templateName: "all",
+      userId: null,
+      meetId: null,
+      sendAt: nDaysAndHoursFromNowInUtcTime(3),
     },
   ]);
 }
