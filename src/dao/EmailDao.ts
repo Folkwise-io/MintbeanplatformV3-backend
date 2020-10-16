dd8c19e5accecd31f12cef7c87b1bc3696e7261b
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-16 14:08:12 -0400

    Set up placeholder methods for EmailDao

--- src/dao/EmailDao.ts
+++ src/dao/EmailDao.ts
@@ -1,8 +1,16 @@
 import sgMail from "@sendgrid/mail";
-import { Email } from "../types/Email";
+import { Email, EmailTemplateName, EmailVars } from "../types/Email";
 
 export class EmailDao {
   constructor(private apiKey: string) {}
+
+  async queue(templateName: EmailTemplateName, emailVars: EmailVars): Promise<void> {
+    throw new Error("Not yet implemented");
+  }
+
+  async deleteScheduledEmail(id: string): Promise<void> {
+    throw new Error("Not yet implemented");
+  }
 
   async sendEmail(email: Email): Promise<boolean> {
     sgMail.setApiKey(this.apiKey);
