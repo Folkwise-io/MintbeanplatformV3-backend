ec9cdbf123f87084472c24097fe9a109bb6bb022
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-16 14:03:12 -0400

    Implement EmailCommander

--- src/service/EmailService/EmailCommander.ts
+++ src/service/EmailService/EmailCommander.ts
@@ -1,14 +1,29 @@
-import sequences from "./sequences";
+import { EmailDao } from "../../dao/EmailDao";
+import { EmailTemplate, EmailTemplateName, EmailVars } from "../../types/Email";
+import MeetRegistrationEmailTemplate from "./templates/MeetRegistrationEmailTemplate";
 
-type EmailHook = "afterRegistration" | "afterSubmission";
+const { MEET_REGISTRATION, WELCOME, ALL, CHECK_IN_AFTER_SIGN_UP } = EmailTemplateName;
+export default class EmailCommander {
+  constructor(private emailDao: EmailDao) {}
+  templates: {
+    [key in EmailTemplateName]: EmailTemplate;
+  } = {
+    [MEET_REGISTRATION]: new MeetRegistrationEmailTemplate(),
+    [WELCOME]: new MeetRegistrationEmailTemplate(),
+    [ALL]: new MeetRegistrationEmailTemplate(),
+    [CHECK_IN_AFTER_SIGN_UP]: new MeetRegistrationEmailTemplate(),
+  };
 
-class EmailObserver {
-  publishEvent(hook: EmailHook) {
-    sequences
-      .find(hook)
-      .order(accordingToStepOrder)
-      .forEach((sequenceStep) => {
-        eventsDatabase.insert({});
-      });
+  // This is called upon triggering inside a controller or service
+  queue(templateName: EmailTemplateName, emailVars: EmailVars) {
+    this.emailDao.queue(templateName, emailVars);
+  }
+
+  // This is called by the cron scheduler
+  async dispatch(id: string, templateName: EmailTemplateName, emailVars: EmailVars) {
+    const template = this.templates[templateName];
+    const email = template.generateEmail(emailVars);
+    this.emailDao.sendEmail(email);
+    this.emailDao.deleteScheduledEmail(id);
   }
 }
