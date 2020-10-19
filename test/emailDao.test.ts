8403a559c1706003739e0db32e43836068870da9
Jimmy Peng <jimmy.peng@mintbean.io>
2020-10-19 18:51:36 -0400

    Add getter tests for emailDao

--- test/emailDao.test.ts
+++ test/emailDao.test.ts
@@ -1,4 +1,5 @@
 import { ScheduledEmail } from "../src/types/Email";
+import { nDaysAndHoursFromNowInUtcTime } from "../src/util/timeUtils";
 import { ALGOLIA } from "./src/meetConstants";
 import { AMY_ALGOLIA_SCHEDULED_EMAIL } from "./src/scheduledEmailConstants";
 import TestManager from "./src/TestManager";
@@ -30,5 +31,21 @@ describe("EmailDao's handling of scheduled emails", () => {
 
     await emailDao.getUnsentScheduledEmails().then((emails: ScheduledEmail[]) => expect(emails).toHaveLength(1));
     await emailDao.getOverdueScheduledEmails().then((emails: ScheduledEmail[]) => expect(emails).toHaveLength(1));
+  });
+
+  it("can queue an email that should be sent in the future and find it", async () => {
+    const SCHEDULED_EMAIL_WITH_DELAY = { ...AMY_ALGOLIA_SCHEDULED_EMAIL, sendAt: nDaysAndHoursFromNowInUtcTime(0, 1) };
+    await emailDao.queue([SCHEDULED_EMAIL_WITH_DELAY]);
+
+    await emailDao.getUnsentScheduledEmails().then((emails: ScheduledEmail[]) => expect(emails).toHaveLength(1));
+    await emailDao.getOverdueScheduledEmails().then((emails: ScheduledEmail[]) => expect(emails).toHaveLength(0));
+  });
+
+  it("does not get emails that are already sent", async () => {
+    const SCHEDULED_EMAIL_ALREADY_SENT = { ...AMY_ALGOLIA_SCHEDULED_EMAIL, sent: true };
+    await emailDao.queue([SCHEDULED_EMAIL_ALREADY_SENT]);
+
+    await emailDao.getUnsentScheduledEmails().then((emails: ScheduledEmail[]) => expect(emails).toHaveLength(0));
+    await emailDao.getOverdueScheduledEmails().then((emails: ScheduledEmail[]) => expect(emails).toHaveLength(0));
   });
 });
