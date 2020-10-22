3c7e8b9f9b7046f832420813c995e2481660583f
Claire Froelich <claire.froelich@gmail.com>
2020-10-22 14:34:13 -0700

    Add kanban seeds and rename all seed files

---
+++ src/db/seeds/21-kanbans.ts
@@ -0,0 +1,26 @@
+import * as Knex from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+  await knex("kanbans").del();
+
+  await knex("kanbans").insert([
+    {
+      id: "00000000-0000-0000-0000-000000000000",
+      title: "Animation Toys 1 Kanban",
+      description: "Building impressive portfolio projects with PaperJS.",
+      createdAt: "2020-10-15",
+    },
+    {
+      id: "00000000-0000-4000-a000-000000000000",
+      title: "Algolia gives you super powers 1 Kanban",
+      description: "Building impressive portfolio projects with PaperJS.",
+      createdAt: "2020-10-15",
+    },
+    {
+      id: "6d32252b-c85c-45d3-8f55-dd05d2e9cfd0",
+      title: "Hack the Hack Kanban",
+      description: "Building impressive portfolio projects with PaperJS.",
+      createdAt: "2020-10-15",
+    },
+  ]);
+}
