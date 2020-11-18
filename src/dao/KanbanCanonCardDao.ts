f288e6e8b79efea2b0f753f6f00b9bbd21e0d4d0
Claire Froelich <claire.froelich@gmail.com>
2020-11-18 11:21:59 -0800

    Add explanation of type in dao

--- src/dao/KanbanCanonCardDao.ts
+++ src/dao/KanbanCanonCardDao.ts
@@ -6,6 +6,8 @@ import {
 } from "../service/KanbanCanonCardService";
 import { KanbanCanonCard } from "../types/gqlGeneratedTypes";
 
+// The service layer needs status and index to orchestrate the update of card positions.
+// status and index do not exist as columns on kanbanCanonCards and these keys are therefore omitted
 export type KanbanCanonCardDaoAddOneInput = Omit<KanbanCanonCardServiceAddOneInput, "status" | "index">;
 export type KanbanCanonCardDaoEditOneInput = Omit<KanbanCanonCardServiceEditOneInput, "status" | "index">;
 
---
+++ src/types/serviceTypes.ts
@@ -0,0 +1,4 @@
+// These are the data types that move data through the backend
+// They may optionally extend gqpGeneratedTypes (indeed this is recommended where possible to prevent type staleness)
+
+/// TODO
