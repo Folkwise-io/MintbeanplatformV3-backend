d563ec04a2721233210ece699afd2084e0c49748
Claire Froelich <claire.froelich@gmail.com>
2020-11-23 15:30:29 -0800

    Remove unused file

--- src/types/serviceTypes.ts
+++
@@ -1,4 +0,0 @@
-// These are the data types that move data through the backend
-// They may optionally extend gqpGeneratedTypes (indeed this is recommended where possible to prevent type staleness)
-
-/// TODO
