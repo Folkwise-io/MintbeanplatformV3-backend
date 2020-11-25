6791ddbf49196fff069a0946e3fd2b16a6252caf
Celeste Ellerby <53948525+theFl00f@users.noreply.github.com>
2020-11-25 18:24:11 -0500

    use alterTable to add default meetType for existing hackathons

--- src/db/migrations/20201125142103_changeMeetTypeToStringOnMeets.ts
+++ src/db/migrations/20201125142103_changeMeetTypeToStringOnMeets.ts
@@ -1,13 +1,23 @@
 import * as Knex from "knex";
 
+//knex raw must be used because alter enum is not supported
+
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.raw(`
+  await knex.schema.raw(`
   ALTER TABLE "meets" DROP CONSTRAINT "meets_meetType_check"
   `);
+  const result = await knex.schema.alterTable("meets", (table) => {
+    table.string("meetType").notNullable().defaultTo("hackathon").alter();
+  });
+  return result;
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.raw(`
+  await knex.schema.alterTable("meets", (table) => {
+    table.enu("meetType", ["hackMeet"]).notNullable().alter();
+  });
+  const result = await knex.schema.raw(`
   ALTER TABLE "meets" ADD CONSTRAINT "meets_meetType_check"
   `);
+  return result;
 }
