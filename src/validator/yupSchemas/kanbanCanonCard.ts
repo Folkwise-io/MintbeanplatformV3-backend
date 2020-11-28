f6c24f539fb78fe5be51f11e1dd29e5bd3cbec97
Claire Froelich <claire.froelich@gmail.com>
2020-11-27 22:21:34 -0800

    Strictor validations

--- src/validator/yupSchemas/kanbanCanonCard.ts
+++ src/validator/yupSchemas/kanbanCanonCard.ts
@@ -1,18 +1,11 @@
 import * as yup from "yup";
-import { KanbanCanonCardStatusEnum } from "../../types/gqlGeneratedTypes";
 
 export const createKanbanCanonCardInputSchema = yup.object().shape({
   title: yup.string().min(2, "Too Short!").max(64, "Too Long!").required(),
   body: yup.string().min(3, "Too Short!").required(),
-  // below are optional
-  status: yup.mixed<KanbanCanonCardStatusEnum>().oneOf(Object.values(KanbanCanonCardStatusEnum)),
-  index: yup.number(),
 });
 
 export const editKanbanCanonCardInputSchema = yup.object().shape({
   title: yup.string().min(2, "Too Short!").max(64, "Too Long!"),
   body: yup.string().min(3, "Too Short!"),
-  // below are optional
-  status: yup.mixed<KanbanCanonCardStatusEnum>().oneOf(Object.values(KanbanCanonCardStatusEnum)),
-  index: yup.number(),
 });
