830a277c8f0275ea13010bf57803d02d13b08adb
Claire Froelich <claire.froelich@gmail.com>
2020-12-07 17:00:42 -0800

    Merge new meetType migrations and add scheduledEmails migration

---
+++ src/db/migrations/20201206153227_scheduledEmails.ts
@@ -0,0 +1,26 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("scheduledEmails", (table) => {
+    table.uuid("id").notNullable().defaultTo(knex.raw("uuid_generate_v4()")).unique();
+    table.text("templateName").notNullable();
+    table.uuid("userRecipientId"); // This will later be converted to a jsonb array field "userRecipientIds" when a use case for multiple user recipients arises
+    table.jsonb("meetRecipientIds");
+    table.uuid("meetId");
+    table.uuid("userId");
+    table.timestamp("sendAt").notNullable().defaultTo(knex.fn.now());
+    table.boolean("sent").notNullable().defaultTo(false);
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+
+    // Constraints and indices
+    table.primary(["id"]);
+    table.foreign("userRecipientId").references("users.id").onDelete("CASCADE");
+    table.foreign("userId").references("users.id").onDelete("SET NULL");
+    table.foreign("meetId").references("meets.id").onDelete("CASCADE");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("scheduledEmails");
+}
