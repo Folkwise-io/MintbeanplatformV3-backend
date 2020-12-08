9115e9a1c6abae66af48408d767c400e79986418
Claire Froelich <claire.froelich@gmail.com>
2020-12-08 12:25:44 -0800

    Commet out typescript bugging ejs render() function for now

--- src/util/templatingUtils.ts
+++ src/util/templatingUtils.ts
@@ -4,9 +4,9 @@ import ejs, { Options } from "ejs";
 type AnyObject = { [key: string]: any };
 
 // TODO: how to return just string (not promise)
-export const render = (string: string, data: AnyObject, options: Options = {}): string => {
-  return ejs.render(string, data, options);
-};
+// export const render = (string: string, data: AnyObject, options: Options = {}): string => {
+//   return ejs.render(string, data, options);
+// };
 
 export const renderFile = (filename: string, data: AnyObject, options: Options = {}): string => {
   return ejs.renderFile(filename, data, options, function (err, str) {
