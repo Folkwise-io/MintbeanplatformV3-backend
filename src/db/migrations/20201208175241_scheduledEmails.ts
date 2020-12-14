50e8106d0cb3a6ee1d648da955cd0f1a76eb8eb8
Claire Froelich <claire.froelich@gmail.com>
2020-12-14 10:04:49 -0800

    Update src/db/migrations/20201208175241_scheduledEmails.ts
    Co-authored-by: Celeste Ellerby <53948525+theFl00f@users.noreply.github.com>

--- src/db/migrations/20201208175241_scheduledEmails.ts
+++ src/db/migrations/20201208175241_scheduledEmails.ts
@@ -11,7 +11,7 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
     table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
 
-    // Constraints and indicesyarn p
+    // Constraints and indices
     table.primary(["id"]);
     table.foreign("userRecipientId").references("users.id").onDelete("CASCADE");
     table.foreign("meetRecipientId").references("meets.id").onDelete("CASCADE");
