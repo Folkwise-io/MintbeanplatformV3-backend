3701dd2f13447c97864c1936e2d42cf497136399
Claire Froelich <claire.froelich@gmail.com>
2020-12-15 16:31:52 -0800

    Add ejs templates

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import { buildExpressServerContext } from "./buildServerContext";
 import buildSchema from "./buildSchema";
 import buildApolloServer from "./buildApolloServer";
 import buildExpressServer from "./buildExpressServer";
+import moment from "moment-timezone";
 
 const persistenceContext = buildPersistenceContext();
 const resolverContext = buildResolverContext(persistenceContext);
 const schema = buildSchema(resolverContext);
 const apolloServer = buildApolloServer(schema, buildExpressServerContext);
 const app = buildExpressServer(apolloServer);
+
+app.locals.moment = moment; // make moment available in ejs templates
 
 app.listen({ port: 4000 }, () => console.log(`Server ready at http://localhost:4000${apolloServer.graphqlPath}`));
